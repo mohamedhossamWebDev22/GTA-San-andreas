f3c57d4498ef34b3be86bcb162dc44c9681e9e8e
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2023-06-23 13:23:16 +0300

    Finish

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,28 @@ import App from './App'
 
 import './style.scss'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRYSOkdCDOioE9j99dXVk4sfQOxBjgyYU",
+  authDomain: "gta-sanandreas-0.firebaseapp.com",
+  projectId: "gta-sanandreas-0",
+  storageBucket: "gta-sanandreas-0.appspot.com",
+  messagingSenderId: "1041058904072",
+  appId: "1:1041058904072:web:fc500b07003b08541c79bd",
+  measurementId: "G-NY5P4PKJTL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const router = createBrowserRouter([
   { 
     path: "/",
